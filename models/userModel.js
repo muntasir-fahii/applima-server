6fdec7e17cab7b8bf1a9138e5ea245262c5a0d29
muntasir-fahim <fa3877957@gmail.com>
2023-03-03 11:34:27 +0600

    validator added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -16,6 +17,22 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All field must be filled");
+  }
+
+  // check if the email is valid
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid email");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Password Is not strong, try to combine uppercase,lowercase, symbol and minimum 8 chars"
+    );
+  }
+
   const exist = await this.findOne({ email });
 
   if (exist) {
